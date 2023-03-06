e99dd3b2491dd98f45b97d6d8cf7a5a4d5299272
Abdelilah Fakhour <Abdelilah.Fakhour@outlook.fr>
2023-03-06 14:44:23 +0100

    retrait code mort ainsi que mise en page

--- recap.php
+++ recap.php
@@ -17,8 +17,8 @@ session_start();
     if (!isset($_SESSION['products']) || empty($_SESSION['products'])) {
         echo "<p>Aucun produit en session...</p>";
     } else {
-        echo "<table class='table table-dark table-striped'>",
-        "  <thead>",
+        echo "<table class='table table-dark table-striped text-center'>",
+        "<thead>",
         "<tr>",
         "<th>#</th>",
         "<th>Nom</th>",
@@ -48,7 +48,7 @@ session_start();
         "</tbody>",
         "</table>";
     }
-    // var_dump($_SESSION);
+
     ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <div class="container m-3">
