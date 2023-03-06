57a0494f9b5ebe68860649bdf865835ccb9e3dee
Abdelilah Fakhour <Abdelilah.Fakhour@outlook.fr>
2023-03-06 16:56:42 +0100

    réorganisation élément

--- index.php
+++ index.php
@@ -35,14 +35,15 @@ $products = findAll();
         }
         echo "</table>";
         ?>
+        <div class="col">
+            <a href="admin.php" class="btn btn-secondary d-block p-2 mt-3">Acceuil</a>
+        </div>
     </div>
 
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-    <div class="col">
-        <a href="admin.php" class="btn btn-secondary d-block p-2 mt-3">Acceuil</a>
-    </div>
+
 </body>
 
 </html>
--- recap.php
+++ recap.php
@@ -13,46 +13,45 @@ session_start();
 </head>
 
 <body class="bg-dark text-light">
-    <h1 class="text-center">Récapitulatifs des produits</h1>
-    <?php
-    if (!isset($_SESSION['products']) || empty($_SESSION['products'])) {
-        echo "<p>Aucun produit en session...</p>";
-    } else {
-        echo "<table class='table table-dark table-striped text-center'>",
-        "<thead>",
-        "<tr>",
-        "<th>#</th>",
-        "<th>Nom</th>",
-        "<th>Prix</th>",
-        "<th>Quantité</th>",
-        "<th>Total</th>",
-        "</tr>",
-        "</thead>",
-        "<tbody>";
+    <div class="container">
+        <h1 class="text-center">Récapitulatifs des produits</h1>
+        <?php
+        if (!isset($_SESSION['products']) || empty($_SESSION['products'])) {
+            echo "<p>Aucun produit en session...</p>";
+        } else {
+            echo "<table class='table table-dark table-striped text-center'>",
+            "<thead>",
+            "<tr>",
+            "<th>#</th>",
+            "<th>Nom</th>",
+            "<th>Prix</th>",
+            "<th>Quantité</th>",
+            "<th>Total</th>",
+            "</tr>",
+            "</thead>",
+            "<tbody>";
 
-        $totalGeneral = 0;
+            $totalGeneral = 0;
 
-        foreach ($_SESSION['products'] as $index => $product) { //parcours le tableau de mes produits stockés en session
+            foreach ($_SESSION['products'] as $index => $product) { //parcours le tableau de mes produits stockés en session
+                echo "<tr>",
+                "<td>" . $index . "</td>",
+                "<td>" . $product['name'] . "</td>", // récupération du nom
+                "<td>" . number_format($product['price'], 2, ",", "&nbsp;") . "&nbsp;€</td>", //récupération du prix
+                "<td>" . '<a href="traitement.php?action=decreaseQuantity&id=' . $index . '" class="btn btn-outline-danger">-</a> ' . $product['qtt'] . ' <a href="traitement.php?action=increaseQuantity&id=' . $index . '" class="btn btn-outline-success">+</a>' . "</td>", //récupération de la quantité
+                "<td>" . number_format($product['total'], 2, ",", "&nbsp;") . "&nbsp;€</td>", //Calcul du total
+                "<td> <a href='traitement.php?action=supprimerProduit&id=$index' class='btn btn-danger'>retirer produit</a></td>",
+                "</tr>";
+                $totalGeneral += $product['total'];
+            }
             echo "<tr>",
-            "<td>" . $index . "</td>",
-            "<td>" . $product['name'] . "</td>", // récupération du nom
-            "<td>" . number_format($product['price'], 2, ",", "&nbsp;") . "&nbsp;€</td>", //récupération du prix
-            "<td>" . '<a href="traitement.php?action=decreaseQuantity&id=' . $index . '" class="btn btn-outline-danger">-</a> ' . $product['qtt'] . ' <a href="traitement.php?action=increaseQuantity&id=' . $index . '" class="btn btn-outline-success">+</a>' . "</td>", //récupération de la quantité
-            "<td>" . number_format($product['total'], 2, ",", "&nbsp;") . "&nbsp;€</td>", //Calcul du total
-            "<td> <a href='traitement.php?action=supprimerProduit&id=$index' class='btn btn-danger'>retirer produit</a></td>",
-            "</tr>";
-            $totalGeneral += $product['total'];
+            "<td colspan=4>Total général : </td>",
+            "<td><srtong>" . number_format($totalGeneral, 2, ",", "&nbsp;") . "&nbsp;€</strong></td>",
+            "</tbody>",
+            "</table>";
         }
-        echo "<tr>",
-        "<td colspan=4>Total général : </td>",
-        "<td><srtong>" . number_format($totalGeneral, 2, ",", "&nbsp;") . "&nbsp;€</strong></td>",
-        "</tbody>",
-        "</table>";
-    }
 
-    ?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-    <div class="container m-3">
+        ?>
         <div class="col">
             <a href="traitement.php?action=viderPanier" class="btn btn-secondary d-block p-2 mt-3">vider panier</a>
         </div>
@@ -61,8 +60,7 @@ session_start();
         </div>
     </div>
 
-
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 
 </body>
 
