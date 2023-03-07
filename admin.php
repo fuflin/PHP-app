d633d01bdf0ae887c62eb8f68154bdf532b13c72
Abdelilah Fakhour <Abdelilah.Fakhour@outlook.fr>
2023-03-07 16:55:59 +0100

    modif esthetique + ajout entrée bdd

--- admin.php
+++ admin.php
@@ -8,13 +8,17 @@
     <title>Ajout produit</title>
 </head>
 
-<!-- Titre du site -->
+<!-- Titre de l'appli -->
 
 <body class="bg-dark text-light">
-    <div class="container mt-5">
-        <h2 class="text-center">Ajouter un produit</h2><br><br><br>
 
-        <form action="traitement.php?action=ajouter" method="post">
+    <div class="container mt-5">
+        <div class="d-flex">
+            <a href="recap.php" class="btn btn-secondary d-block">Recap</a>
+            <a href="index.php" class="btn btn-secondary d-block ms-3">Index BD</a>
+        </div>
+        <h2 class="text-center mb-5">Ajouter un produit</h2>
+        <form action="traitement.php?action=ajoutbdd" method="post">
             <div class="row">
                 <!-- champ pour le nom du produit -->
                 <div class="col">
@@ -24,22 +28,20 @@
                 <!-- champ pour le prix du produit -->
                 <div class="col">
                     <label for="price" class="form-label">Prix du produit :</label>
-                    <input type="text" class="form-control" step="any" name="price">
+                    <input type="text" class="form-control" name="price">
                 </div>
-                <!-- champ pour la quantité du produit -->
-                <div class="col">
-                    <label for="qtt" class="form-label">Quantité desirée :</label>
-                    <input type="number" class="form-control" name="qtt" value="1">
-                </div>
+            </div>
+            <!-- champ pour la quantité du produit -->
+            <div class="d-flex flex-column align-items-center mt-5">
+                <label for="descr" class="form-label">Description :</label>
+                <textarea name="descr" class="w-50 "></textarea>
             </div>
             <!-- bouton pour ajouter le produit saisie -->
-            <div class="d-grid gap-2 mt-3">
-                <input type="submit" class="btn btn-secondary" name="submit" value="Ajouter le produit">
+            <div class="d-flex justify-content-center mt-5">
+                <input type="submit" class="btn btn-secondary w-50" name="submit" value="Ajouter le produit">
             </div>
         </form>
-        <!-- bouton annexe -->
-        <a href="recap.php" class="btn btn-secondary d-block p-2 mt-3">Panier</a>
-        <a href="index.php" class="btn btn-secondary d-block p-2 mt-3">Index BD</a>
+
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
